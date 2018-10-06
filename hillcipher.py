a48552d630029b408fd77c5ed896bb1419a6bca5
Jason Benner <jbenner1984@gmail.com>
2018-10-06 11:44:09 -0400

    Update hillcipher.py

--- hillcipher.py
+++ hillcipher.py
@@ -1,2 +1,18 @@
-import math
-import random
+import numpy
+
+#Set up the alphabet
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#Creata a martrix that we want to use as they key for our encoding, we will use 2X2 smatrix for now
+encodingMatrix = np.array([2,5],[7,8])
+
+#Now we need to create the message we want to encode, we would get user input, put all lower and remove spaces. Will need to be multiple
+#of two for now to ease of use. Later create way to do any length with padding
+message = "attackatdawn"
+
+#Split message into arrays of two letters and perform encoding
+for (first, second) in zip(message[0::2], message[1::2]):
+    print first, second
+  
+
+
