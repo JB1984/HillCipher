15ee3ff973f5b0074b726a536fe99ee82b93a174
Jason Benner <jbenner1984@gmail.com>
2018-10-14 18:01:22 -0400

    Add files via upload

--- hillcipher.py
+++ hillcipher.py
@@ -17,31 +17,58 @@ def encryptPassage(message, alphabet):
     print(encodedMessage)
     return encodedMessage
 
+
+### Get modulo reciprocals ###
+
+def getModuloRecip(alphabet):
+
+    recipModuloDict = {}
+
+    length = len(alphabet)
+
+    for x in range(0,length):
+        for y in range(0, length):
+            number = x*y
+            if number % length == 1:
+                recipModuloDict[x] = y
+            continue
+        continue
+
+    return recipModuloDict
+
 ### DECODING ###
 
-def decryptPassage(message, encodingMatrix):
+def decryptPassage(message, encodingMatrix, alphabet):
     
-    #Find the determinate of the enciphering matrix
-    determinate1 = encodingMatrix[0][0]*encodingMatrix[1][1]
-    determinate2 = encodingMatrix[0][1]*encodingMatrix[1][0]
-    determinateFinal = determinate1-determinate2
+    #Find the determinate of the enciphering matrix basic method
+    #determinate1 = encodingMatrix[0][0]*encodingMatrix[1][1]
+    #determinate2 = encodingMatrix[0][1]*encodingMatrix[1][0]
+    #determinateFinal = determinate1-determinate2
 
-    recipModuloDict = {
-        1:1,
-        3:9,
-        5:21,
-        7:15,
-        9:3,
-        11:19,
-        15:7,
-        17:23,
-        19:11,
-        21:5,
-        23:17,
-        25:25,
-    }
+    #Find the determinate using Numpy
+    determinateFinal = round(np.linalg.det(encodingMatrix))
+    print(determinateFinal)
+
+    #recipModuloDict = {
+    #    1:1,
+    #    3:9,
+    #    5:21,
+    #    7:15,
+    #    9:3,
+    #    11:19,
+    #    15:7,
+    #    17:23,
+    #    19:11,
+    #    21:5,
+    #    23:17,
+    #    25:25,
+    #}
+
+    recipModuloDict = getModuloRecip(alphabet)
+    print(recipModuloDict)
 
     repModuloValue = recipModuloDict[abs(determinateFinal)]
+    print(determinateFinal**-1%len(alphabet))
 
     newMatrix = np.array([[repModuloValue*encodingMatrix[1][1],
                         -repModuloValue*encodingMatrix[0][1]],
@@ -72,7 +99,7 @@ def decryptPassage(message, encodingMatrix):
 encodingMatrix = np.array([[5, 5], [3, 8]])
 
 #Set up the alphabet
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '.', '?', ' ']
 
 choiceOfRun = ''
 
@@ -84,7 +111,7 @@ while choiceOfRun != "A" or "B" or "Q":
 
         # Encryption
         passageInput = input("Enter passage you wish to encrypt: ")
-        passageInput = passageInput.lower().replace(" ", "")
+        passageInput = passageInput.lower()
 
         encryptedPassage = encryptPassage(passageInput, alphabet)
 
@@ -100,9 +127,9 @@ while choiceOfRun != "A" or "B" or "Q":
             
         # Decryption
         encryptedPassageInput = input("Enter passage you wish to decrypt: ")
-        encryptedPassageInput = encryptedPassageInput.lower().replace(" ", "")
+        encryptedPassageInput = encryptedPassageInput.lower()
 
-        decryptedPassage = decryptPassage(encryptedPassageInput, encodingMatrix)
+        decryptedPassage = decryptPassage(encryptedPassageInput, encodingMatrix, alphabet)
 
         decryptedPassageString = ''.join(decryptedPassage)
 
